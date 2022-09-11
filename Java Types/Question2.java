3f99ef51086362a0b2b43e5a7e0cefafc72cfc72
Dylan-Bellamy <90278426+Dylan-Bellamy@users.noreply.github.com>
2022-09-11 10:21:12 -0400

    Homework Java Types
    This was completed on 2022-09-11 and I found this fairly easy because I experience in python.
    
    Question #4 Answers
    
    1. 5
    2. his
    3. hi 8
    4. hj

---
+++ Java Types/Question2.java
@@ -0,0 +1,17 @@
+
+class Question2 {
+
+    static String hannah = "Did Hannah see bees? Hannah did.";
+    static int hannahLength = hannah.length();
+    static char hannahChar = hannah.charAt(12);
+    static char hannahChar2 = hannah.charAt(15);
+
+    public static void main(String[] args) {
+        
+        System.out.print(hannahLength);
+        System.out.print(hannahChar);
+        System.out.print(hannahChar2);
+
+    }
+}
+ 
